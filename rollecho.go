2a6202e5d758e83858eb43b77f87197ea5ec9d61
d-smith <doug.smith@fmr.com>
2016-02-25 13:59:56 -0800

    Added oauth2 callback and echo client page

--- rollecho.go
+++ rollecho.go
@@ -1,27 +1,42 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	az "github.com/xtraclabs/roll/authzwrapper"
 	"github.com/xtraclabs/roll/repos"
+	"html/template"
 	"io/ioutil"
+	"log"
 	"net/http"
+	"net/url"
 	"os"
 )
+
+var jsonResponse accessTokenResponse
+
+var templates = template.Must(template.ParseFiles("./echo.html"))
+
+type accessTokenResponse struct {
+	AccessToken string `json:"access_token"`
+	TokenType   string `json:"token_type"`
+}
 
 func echoHandler() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		switch r.Method {
-		case "PUT":
-			body, err := ioutil.ReadAll(r.Body)
+		case "POST":
+			/*body, err := ioutil.ReadAll(r.Body)
 			if err != nil {
 				w.WriteHeader(http.StatusInternalServerError)
 				return
 			}
 			r.Body.Close()
-			w.Write(body)
-			w.Write([]byte("\n"))
+			*/
+			echoTxt := r.FormValue("echo")
+			log.Println("data to echo:", echoTxt)
+			w.Write([]byte(echoTxt))
 		default:
 			w.WriteHeader(http.StatusMethodNotAllowed)
 		}
@@ -30,6 +45,58 @@ func echoHandler() http.HandlerFunc {
 
 func readWhitelistClientIDFromEnv() string {
 	return os.Getenv("ECHO_WHITELISTED_CLIENT_ID")
+}
+
+func oauthCallbackHandler() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		params := r.URL.Query()
+		codes := params["code"]
+
+		resp, err := http.PostForm(os.Getenv("ROLL_ENDPOINT")+"/oauth2/token",
+			url.Values{"grant_type": {"authorization_code"},
+				"code": {codes[0]}, "client_id": {os.Getenv("ECHO_WHITELISTED_CLIENT_ID")},
+				"client_secret": {os.Getenv("CLIENT_SECRET")},
+				"redirect_uri":  {os.Getenv("REDIRECT_URI")}})
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		defer resp.Body.Close()
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		log.Println(fmt.Sprintf("response body: %v", string(body)))
+
+		err = json.Unmarshal(body, &jsonResponse)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		http.Redirect(w, r, "/echoclient", http.StatusFound)
+
+		w.Write([]byte("now what?"))
+	}
+}
+
+func handleEcho() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case "GET":
+
+			if err := templates.ExecuteTemplate(w, "echo.html", jsonResponse); err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+		default:
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		}
+	})
 }
 
 func main() {
@@ -43,6 +110,8 @@ func main() {
 	var whitelisted = readWhitelistClientIDFromEnv()
 
 	mux := http.NewServeMux()
-	mux.Handle("/echo", az.Wrap(repos.NewVaultSecretsRepo(), repos.NewDynamoAdminRepo(), []string{whitelisted}, echoHandler()))
+	mux.Handle("/echoclient", handleEcho())
+	mux.Handle("/oauth2_callback", oauthCallbackHandler())
+	mux.Handle("/echosvc", az.Wrap(repos.NewVaultSecretsRepo(), repos.NewDynamoAdminRepo(), []string{whitelisted}, echoHandler()))
 	http.ListenAndServe(fmt.Sprintf(":%d", *port), mux)
 }
--- setenv.sh
+++ setenv.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 export VAULT_ADDR=http://localhost:8200
 export VAULT_TOKEN=6efc4030-fda6-79ef-39a9-40f6a546436e
-export ECHO_WHITELISTED_CLIENT_ID=d80c9758-f965-4286-7003-e5af065d8082
+export ECHO_WHITELISTED_CLIENT_ID=0138d6ba-de55-435c-6e91-588f6a866f46
+export CLIENT_SECRET=1V8DEkit8dmDxmS2Vw6gUbKXvoiuIQDqSVSW6sJv4jE=
+export REDIRECT_URI=http://localhost:5000/oauth2_callback
+export ROLL_ENDPOINT=http://localhost:3000
